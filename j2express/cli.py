78a299d1df1fc06101e2475a0f544824afdd7916
JP Etcheber <jetcheber@gmail.com>
2023-12-11 01:03:48 -0800

    function reordering

--- j2express/cli.py
+++ j2express/cli.py
@@ -14,6 +14,18 @@ def find_upper_case_envs(line):
     return [f"{{{{ {x} }}}}" for x in matches]
 
 
+def process_file(filename):
+    with open(filename, 'r') as f:
+        template_lines = f.readlines()
+    for i in range(0, len(template_lines)):
+        env_vars = find_upper_case_envs(template_lines[i])
+        for e_var in env_vars:
+            clean_evar = re.sub(r"[{}\s]", "", e_var)
+            template_lines[i] = template_lines[i].replace(
+                e_var, f"{{{{ \"\" | enviro_value('{clean_evar}') }}}}")
+    return template_lines
+
+
 @click.command()
 @click.option('-f', '--filename', help='J2 Template to process')
 @click.option('--trim-blocks/--no-trim-blocks', show_default=True,
@@ -25,14 +37,7 @@ def find_upper_case_envs(line):
 def main(filename, lstrip_blocks, trim_blocks):
     env = Environment(lstrip_blocks=lstrip_blocks, trim_blocks=trim_blocks)
     env.filters['enviro_value'] = enviro_value
-    with open(filename, 'r') as f:
-        template_lines = f.readlines()
-    for i in range(0, len(template_lines)):
-        env_vars = find_upper_case_envs(template_lines[i])
-        for e_var in env_vars:
-            clean_evar = re.sub(r"[{}\s]", "", e_var)
-            template_lines[i] = template_lines[i].replace(
-                e_var, f"{{{{ \"\" | enviro_value('{clean_evar}') }}}}")
+    template_lines = process_file(filename)
     template = env.from_string("".join(template_lines))
     click.echo(template.render())
 
